a3a2c815e134c3719696725338977840e9598ac7
jaspreet_suri <160199423+SKTECHZ@users.noreply.github.com>
2024-04-16 15:18:05 +0530

    our Client and our work section

--- components/Home/OurClients.jsx
+++ components/Home/OurClients.jsx
@@ -1,7 +1,153 @@
 import React from "react";
 import styles from "./styles.module.scss";
+import Image from "next/image";
 const OurClients = () => {
-  return <div>OurClients</div>;
+  const ourClientData = {
+    heading: "Our Clients",
+    cards: [
+      {
+        img: {
+          src: "/image/OurClients/facebook.png",
+          alt: "facebook",
+        },
+      },
+      {
+        img: {
+          src: "/image/OurClients/Microsoft.png",
+          alt: "Microsoft",
+        },
+      },
+      {
+        img: {
+          src: "/image/OurClients/mash.png",
+          alt: "mash",
+        },
+      },
+      {
+        img: {
+          src: "/image/OurClients/google.png",
+          alt: "google",
+        },
+      },
+      {
+        img: {
+          src: "/image/OurClients/facebook.png",
+          alt: "facebook",
+        },
+      },
+      {
+        img: {
+          src: "/image/OurClients/Microsoft.png",
+          alt: "Microsoft",
+        },
+      },
+      {
+        img: {
+          src: "/image/OurClients/mash.png",
+          alt: "mash",
+        },
+      },
+      {
+        img: {
+          src: "/image/OurClients/google.png",
+          alt: "google",
+        },
+      },
+      {
+        img: {
+          src: "/image/OurClients/facebook.png",
+          alt: "facebook",
+        },
+      },
+      {
+        img: {
+          src: "/image/OurClients/Microsoft.png",
+          alt: "Microsoft",
+        },
+      },
+      {
+        img: {
+          src: "/image/OurClients/mash.png",
+          alt: "mash",
+        },
+      },
+      {
+        img: {
+          src: "/image/OurClients/google.png",
+          alt: "google",
+        },
+      },
+      {
+        img: {
+          src: "/image/OurClients/facebook.png",
+          alt: "facebook",
+        },
+      },
+      {
+        img: {
+          src: "/image/OurClients/Microsoft.png",
+          alt: "Microsoft",
+        },
+      },
+      {
+        img: {
+          src: "/image/OurClients/mash.png",
+          alt: "mash",
+        },
+      },
+      {
+        img: {
+          src: "/image/OurClients/google.png",
+          alt: "google",
+        },
+      },
+      {
+        img: {
+          src: "/image/OurClients/facebook.png",
+          alt: "facebook",
+        },
+      },
+      {
+        img: {
+          src: "/image/OurClients/Microsoft.png",
+          alt: "Microsoft",
+        },
+      },
+      {
+        img: {
+          src: "/image/OurClients/mash.png",
+          alt: "mash",
+        },
+      },
+      {
+        img: {
+          src: "/image/OurClients/google.png",
+          alt: "google",
+        },
+      },
+    ],
+  };
+  return (
+    <div className={styles.OurClients}>
+      <div className={styles.OurClients__header}>
+        <h2>{ourClientData.heading}</h2>
+      </div>
+      <ul className={styles.OurClients__lists}>
+        {ourClientData.cards.map((card, index) => {
+          return (
+            <li key={index} className={styles.OurClients__lists__card}>
+              <Image
+                src={card.img.src}
+                alt={card.img.alt}
+                width={200}
+                height={200}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default OurClients;
--- components/Home/OurWork.jsx
+++ components/Home/OurWork.jsx
@@ -1,90 +1,153 @@
 import React from "react";
 import styles from "./styles.module.scss";
+import Image from 'next/image';
 const OurWork = () => {
   const ourWorkData = {
-    heading:"",
+    heading:"Our Work",
     cards:[
       {
         img:{
-          url:"../../public/image/OurWork/work_1.png",
+          url:"/image/OurWork/work_1.png",
           alt:"work_1"
         }
       },
       {
         img:{
-          url:"../../public/image/OurWork/work_2.png",
+          url:"/image/OurWork/work_2.png",
           alt:"work_2"
         }
       },
       {
         img:{
-          url:"../../public/image/OurWork/work_1.png",
+          url:"/image/OurWork/work_1.png",
           alt:"work_1"
         }
       },
       {
         img:{
-          url:"../../public/image/OurWork/work_2.png",
+          url:"/image/OurWork/work_2.png",
           alt:"work_2"
         }
       },
       {
         img:{
-          url:"../../public/image/OurWork/work_1.png",
+          url:"/image/OurWork/work_6.png",
+          alt:"work_6"
+        }
+      },
+      {
+        img:{
+          url:"/image/OurWork/work_3.png",
+          alt:"work_3"
+        }
+      },
+      {
+        img:{
+          url:"/image/OurWork/work_4.png",
+          alt:"work_4"
+        }
+      },
+      {
+        img:{
+          url:"/image/OurWork/work_6.png",
+          alt:"work_6"
+        }
+      },
+      {
+        img:{
+          url:"/image/OurWork/work_3.png",
+          alt:"work_3"
+        }
+      },
+      {
+        img:{
+          url:"/image/OurWork/work_4.png",
+          alt:"work_4"
+        }
+      },
+      {
+        img:{
+          url:"/image/OurWork/work_7.png",
+          alt:"work_7"
+        }
+      },
+      {
+        img:{
+          url:"/image/OurWork/work_5.png",
+          alt:"work_5"
+        }
+      },
+      {
+        img:{
+          url:"/image/OurWork/work_6.png",
+          alt:"work_6"
+        }
+      },
+      {
+        img:{
+          url:"/image/OurWork/work_7.png",
+          alt:"work_7"
+        }
+      },
+      {
+        img:{
+          url:"/image/OurWork/work_1.png",
           alt:"work_1"
         }
       },
       {
         img:{
-          url:"",
-          alt:""
+          url:"/image/OurWork/work_2.png",
+          alt:"work_2"
         }
       },
       {
         img:{
-          url:"",
-          alt:""
+          url:"/image/OurWork/work_1.png",
+          alt:"work_1"
         }
       },
       {
         img:{
-          url:"",
-          alt:""
+          url:"/image/OurWork/work_2.png",
+          alt:"work_2"
         }
       },
       {
         img:{
-          url:"",
-          alt:""
+          url:"/image/OurWork/work_1.png",
+          alt:"work_1"
         }
       },
       {
         img:{
-          url:"",
-          alt:""
-        }
-      },
-      {
-        img:{
-          url:"",
-          alt:""
-        }
-      },
-      {
-        img:{
-          url:"",
-          alt:""
-        }
-      },
-      {
-        img:{
-          url:"",
-          alt:""
+          url:"/image/OurWork/work_2.png",
+          alt:"work_2"
         }
       },
     ]
   }
-  return <div>OurWork</div>;
+  return (
+    <div className={styles.OurWork}>
+      <div className={styles.OurWork__header}>
+        <h2>{ourWorkData.heading}</h2>
+      </div>
+      <ul className={styles.OurWork__lists}>
+        {ourWorkData.cards.map((card,index) => {
+          return (
+            <li key={index} className={styles.OurWork__lists__card}>
+              <Image
+                src={card.img.url}
+                alt={card.img.alt}
+                width={200}
+                height={200}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default OurWork;
